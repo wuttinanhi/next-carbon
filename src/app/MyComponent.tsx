eac41006d2aeaa0a80861cb74472367425f911d5
wuttinanhi <42175957+wuttinanhi@users.noreply.github.com>
2023-06-01 00:30:36 +0700

    no type on Button import :dizzy_face:

---
+++ src/app/MyComponent.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+// @ts-ignore - Carbon components are not typed
+import { Button } from "@carbon/react";
+
+export function MyComponent() {
+  return <Button>Example usage</Button>;
+}
